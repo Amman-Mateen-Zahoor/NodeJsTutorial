1f807cbe288b593fbff2d6373ea7a7ef9ebfa62a
Amman-Mateen-Zahoor <amman.W4u@gmail.com>
2025-02-03 12:52:33 +0500

    Express Practice start

---
+++ FirstApiData.js
@@ -0,0 +1,7 @@
+const data = [
+    {name:"Amman",regNo:'2020-arid-3602'},
+    {name:"Amman",regNo:'2020-arid-3602'},
+    {name:"Amman",regNo:'2020-arid-3602'},
+
+]
+module.exports=data
--- index.js
+++ index.js
@@ -1,5 +1,37 @@
-const http =require('http')
-http.createServer((req,res)=>{
-    res.write('<h1>HI Welcome To NodeJS Serve</h1>')
-    res.end()
-}).listen(4500)
+// const http =require('http')
+// http.createServer((req,res)=>{
+//     res.write('<h1>HI Welcome To NodeJS Serve</h1>')
+//     res.end()
+// }).listen(4500)
+
+// const chalk = require('chalk')
+// import('chalk').then((chalk) => {
+//     console.log(chalk.default.blue('kkkkkkkkko'));
+//   });
+
+// First Api 
+// const data = require('./FirstApiData')
+// const http =require('http')
+// http.createServer((req,res)=>{
+// res.writeHead(200,{'content-type':'application\json'})
+// res.write(JSON.stringify(data))
+// res.end()
+// }).listen(4500)  
+
+// Express Start 
+const Express = require('express')
+const app = Express()
+
+app.get('',(req,res)=>{
+  
+    res.send("Weolcome to Home Page")
+})
+app.get('/about',(req,res)=>{
+  
+    res.send("Weolcome to About Page")
+})
+app.get('/contactUs',(req,res)=>{
+  
+    res.send("Weolcome to ContactUs Page")
+})
+app.listen(4500)
